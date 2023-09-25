c7ab32271cd1270d94e071e7519d06e98f708e90
Lillian/Ace Thompson <114019309+dumb3mok1d@users.noreply.github.com>
2023-09-25 14:56:51 -0500

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues_Thompson_Lillian
+{
+    internal class Player
+    { 
+        public int Health { get; set; }
+
+        public string GamerTag { get; } 
+
+        public Player(int healthParam, string nameParam)
+        {
+            Health = healthParam;
+            GameTag = nameParam;
+        }
+
+        public void JoinGame(Game game, Queue<Player>)
+        {
+            if (game.CurrentPlayers == game.MaxPlayers)
+            {
+                Queue.Enqueue(this);
+            }
+            else
+            {
+                Console.WriteLine("Joining Game!!");
+                game.CurrentPlayers++;
+            }
+            
+                public override string ToString()
+        {
+            return GamerTag;
+        }
+        
+
+       
+         }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Queues_Thompson_Lillian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game myGame = new Game(50, 49);
+
+            Queue<Player> waitingToJoinQ = new Queue<Player>();
+
+            Player p1 = new Player(100, "MMsix");
+            Player p2 = new Player(100, "XxAwesomenessXx");
+            Player p3 = new Player(100, "Get Good");
+            Player p4 = new Player(100, "ZombieSlaya");
+
+            p1.JoinGame(myGame, waitingToJoinQ);
+            p2.JoinGame(myGame, waitingToJoinQ);
+            p3.JoinGame(myGame, waitingToJoinQ);
+            p4.JoinGame(myGame, waitingToJoinQ);
+
+            Console.WriteLine("Player in DA Q:");
+            foreach (Player player in waitingToJoinQ)
+            {
+                Console.WriteLine(player);
+            }
+            myGame.KickPlayer();
+            myGame.CheckQueue(waitingToJoinQ);
+
+            Console.WriteLine("/nPLayers in da q:");
+            foreach(Player player in waitingToJoinQ)
+            {
+                Console.WriteLine(player);
+            }
+           
+
+        }
+    }
+}
